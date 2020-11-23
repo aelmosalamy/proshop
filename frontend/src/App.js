6b0ff2cf2400998b8868b2c192dd80ebb9a1da8d
Adham Elmosalamy <adham@elmosalamy.net>
2020-11-23 14:19:24 +0400

    Set up react-bootstrap and add product imageS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
