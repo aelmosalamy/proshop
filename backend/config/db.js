8c45ddb7dafafcd015ebc90044afe16c65973fc9
Adham Elmosalamy <adham@elmosalamy.net>
2020-11-25 01:54:39 +0400

    Create DB config/connection and models

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+
+    console.log(
+      `Connected to MongoDB successfully: ${conn.connection.host}`.cyan.bold
+    )
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
+// Expose process.env
 dotenv.config()
+
+// Connect to MongoDB
+connectDB()
 
 const app = express()
 
@@ -26,5 +32,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
