7399d44571790498dad27590408765e30125b59d
Adham Elmosalamy <adham@elmosalamy.net>
2020-12-01 03:41:16 +0400

    Added Add To Cart option

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useHistory, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProductDetails } from '../actions/productActions'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = () => {
+  // React Router hooks
   const { id } = useParams()
-  const dispatch = useDispatch()
+  const history = useHistory()
 
+  // Component state hooks
+  const [qty, setQty] = useState(0)
+
+  // Redux hooks
+  const dispatch = useDispatch()
   const { loading, error, product } = useSelector(state => state.productDetails)
 
+  // React useEffect hook to re-render component based on dependencies's change
   useEffect(() => {
     dispatch(listProductDetails(id))
-    /* changes in id as well as initial load will trigger useEffect, I am not sure why you need to add id so, since changes in id variable are synonymous with page load, idk */
-  }, [id])
+  }, [id, dispatch])
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -70,8 +80,31 @@ const ProductScreen = ({ match }) => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={e => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map(x => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className='btn-block'
                     type='button'
                     disabled={product.countInStock === 0}
