fe5a156d8fbc32b149e31e714be99f7348ee6853
Adham Elmosalamy <adham@elmosalamy.net>
2020-12-01 22:51:17 +0400

    Refactoring routes, separating routing and controller logic

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,40 +1,13 @@
 import express from 'express'
 import Product from '../models/productModel.js'
-import colors from 'colors'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
 const router = express.Router()
 
-// @desc   Fetch all products
-// @route  GET /api/products
-// @access Public
-router.get('/', (req, res, next) => {
-  Product.find({})
-    .then(products => {
-      res.json(products)
-    })
-    .catch(error => {
-      // Calling next with error to pass it to the error handling middleware
-      next(error)
-    })
-})
-
-// @desc   Fetch singe product
-// @route  GET /api/product/:id
-// @access Public
-router.get('/:id', (req, res, next) => {
-  Product.findById(req.params.id)
-    .then(product => {
-      if (product) {
-        res.json(product)
-      } else {
-        res.status(404)
-        throw new Error('Product Not Found')
-      }
-    })
-    .catch(error => {
-      // Calling next with error to pass it to the error handling middleware
-      next(error)
-    })
-})
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
